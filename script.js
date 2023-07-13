e877d516ba5f4773d603586ede9bf19e5e9a9f9a
Vishal <93901780+Vish-19@users.noreply.github.com>
2023-07-13 11:16:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text", {
+    strings: ["Data Science", "Machine Learning", "Artificial Intelligence"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
